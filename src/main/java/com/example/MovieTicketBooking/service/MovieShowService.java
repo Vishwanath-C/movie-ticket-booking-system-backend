0782c1b01a91c900ce86df88a945c0122def5464
Vishwanath-C <vishwanath.kardvg@gmail.com>
2025-08-11 21:29:58 +0530

    Update MovieShowService.java

--- src/main/java/com/example/MovieTicketBooking/service/MovieShowService.java
+++ src/main/java/com/example/MovieTicketBooking/service/MovieShowService.java
@@ -68,7 +68,7 @@ public class MovieShowService
 
             for (MovieShow movieShow : movieShows) {
                 MovieShowResponseDto dto = movieShowResponseDtoMapper.convertToMovieShowResponseDto(movieShow);
-                response.getLast().add(dto);
+                response.get(response.size() - 1).add(dto);
             }
         }
         return response;
